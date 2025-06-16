030d0cca0892035e6682daff3f402a5c6204a7e8
nestfeld <nestfeld.quick@gmail.com>
2025-06-16 05:53:49 +0500

    Update translateText.jsx

--- src/components/TextTranslator/translateText.jsx
+++ src/components/TextTranslator/translateText.jsx
@@ -1,5 +1,5 @@
 export const translateText = async (text) => {
-  const apiKeyTranslate = "AIzaSyDaCqidD4jgpMwkRBOypadl2leDhm0KUXM"; // API-ключ для Google Translate
+  const apiKeyTranslate = "AIzaSyA-kk44XfxazRCBBgYzT6FSHChOxS5uIHk"; // API-ключ для Google Translate
   const url = `https://translation.googleapis.com/language/translate/v2`;
   const params = new URLSearchParams({
     q: text,
