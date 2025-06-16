b27ddeb59f8a40e4c9eaea757e10b48739f3dc60
nestfeld <nestfeld.quick@gmail.com>
2025-06-16 05:58:53 +0500

    Update translateText.jsx

--- src/components/TextTranslator/translateText.jsx
+++ src/components/TextTranslator/translateText.jsx
@@ -1,5 +1,5 @@
 export const translateText = async (text) => {
-  const apiKeyTranslate = "AIzaSyA-kk44XfxazRCBBgYzT6FSHChOxS5uIHk"; // API-ключ для Google Translate
+  const apiKeyTranslate = process.env.VITE_TRANSLATE_API_KEY; // API-ключ для Google Translate
   const url = `https://translation.googleapis.com/language/translate/v2`;
   const params = new URLSearchParams({
     q: text,
